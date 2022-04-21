080106443cf28961b5836e3ae05f2a9ea7ff0a20
Pablo <pab.a.bari@gmail.com>
2022-04-21 20:58:00 -0300

    Introduccion a Go

---
+++ go/src/curso-go/01-Introduccion/02-Variables/variables.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nombre string
+	nombre = "Pablo"
+
+	var apellido string = "Bari"
+
+	edad := 39
+
+	fmt.Println(nombre, apellido, edad)
+
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	const pi = 3.141592
+
+	fmt.Println(pi)
+}
